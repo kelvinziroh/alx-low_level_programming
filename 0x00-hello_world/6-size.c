1c765baaa2e8cd2eac53a608a12ef9f7c8850c71
kelvinziroh <kelvin.ziroh@gmail.com>
2022-09-11 07:19:36 -0700

    Edit code formatting according to betty linter

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,6 +12,7 @@ int main(void)
 	long int num;
 	long long int big_num;
 	float f;
+
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(num));
