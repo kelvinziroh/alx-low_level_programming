32defcd5fd5bdb1df575e524b24b8d4e046dee52
kelvinziroh <kelvin.ziroh@gmail.com>
2022-09-11 07:17:21 -0700

    Add float data type to 6-size.c program

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,11 +11,12 @@ int main(void)
 	char c;
 	long int num;
 	long long int big_num;
-
+	float f;
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(num));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)
 			sizeof(big_num));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
