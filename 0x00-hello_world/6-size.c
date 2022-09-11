dd442f94a4d00368fc19609901145921cd5bccb1
kelvinziroh <kelvin.ziroh@gmail.com>
2022-09-11 07:12:38 -0700

    Print sizes of different data types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Print out sizes of different types
+ *
+ * Return: 0 if program executes successfully
+ */
+int main(void)
+{
+	int i;
+	char c;
+	long int num;
+	long long int big_num;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(num));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)
+			sizeof(big_num));
+	return (0);
+}
