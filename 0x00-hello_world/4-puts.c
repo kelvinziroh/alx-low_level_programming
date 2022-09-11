8008131ff9dd8b0674aac58f331b88e43d70a5f1
kelvinziroh <kelvin.ziroh@gmail.com>
2022-09-11 06:08:42 -0700

    Print a string to standard output using puts()

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Print a string to standard output
+ *
+ * Return: 0 if program executes successfully
+ */
+int main(void)
+{
+	char quote[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(quote);
+	return (0);
+}
