2f2b995578bff1f2d6defa2a7b60e770148ae5d0
kelvinziroh <kelvin.ziroh@gmail.com>
2022-09-12 13:14:45 -0700

    Check if random number is zero, +ve or -ve

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Assign random number to variable n
+ * and check if it is 0, positive or negative
+ *
+ * Return: 0 if program executes successfully
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n == 0)
+		printf("%i is zero\n", n);
+	else if (n > 0)
+		printf("%i is positive\n", n);
+	else
+		printf("%i is negative\n", n);
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,3 @@
+# C - Variables, if, else, while
+
+This directory contains C programs that use variables, control and loop statements.
