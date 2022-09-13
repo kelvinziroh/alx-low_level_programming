23f5ab8065d3fc8bd491b6cc2d02c0bedf980e28
kelvinziroh <kelvin.ziroh@gmail.com>
2022-09-12 17:53:32 -0700

    Print numbers between 0 and 9 using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Print integers from 0 to 9
+ *
+ * Return: 0 if program runs successfully
+ */
+int main(void)
+{
+	int count;
+
+	for (count = 48; count < 58; count++)
+	{
+		putchar(count);
+	}
+	putchar("\n");
+
+	return (0);
+}
