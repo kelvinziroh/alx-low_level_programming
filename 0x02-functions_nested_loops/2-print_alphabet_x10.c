aa462ccf9987231e1fa6d2e0574db259f66085c6
kelvinziroh <kelvin.ziroh@gmail.com>
2022-09-15 20:10:35 -0700

    Print alphabets 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Print alphabet 10 times
+ *
+ * Return: 0 if program executes successfully
+ */
+void print_alphabet_x10(void)
+{
+	int i,j;
+
+	i = 0;
+	while (i < 10)
+	{
+		j = 97;
+		while (j < 123)
+		{
+			_putchar(j);
+			j++;
+		}
+		_putchar('\n');
+	}
+}
