b9d33f84d5179d424a3d020a181f72720fcb72f6
kelvinziroh <kelvin.ziroh@gmail.com>
2022-09-15 21:19:50 -0700

    Display absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * -abs - Display the absolute value of an integer
+ *
+ *  @i: Input number as integer
+ *
+ *  Return: Integer's absolute value
+ */
+int _abs(int i)
+{
+	if (i >= 0)
+		return (i);
+	else
+		return (i * -1);
+}
