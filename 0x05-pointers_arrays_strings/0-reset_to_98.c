5129d91ab8c0a3929d63dfbc5009263fdc034f6a
kelvinziroh <kelvin.ziroh@gmail.com>
2022-09-19 12:09:43 -0700

    Change the value of a parameter using a pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - update a value to a parameter using a pointer
+ * @n: pointer variable
+ *
+ * Return: Nothing.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+# Pointers, Arrays and Strings
+
+This directory contains C programs that use pointers, arrays and strings.
