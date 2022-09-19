27aec293c51b71ca738687e3af0b52c8c6b6295e
kelvinziroh <kelvin.ziroh@gmail.com>
2022-09-19 12:35:34 -0700

    Rename the function

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _strlength - Computes the length of a string.
+ * _strlen - Computes the length of a string.
  * @s: pointer variable to the string.
  *
  * Return: integer representing length of a string.
  */
-int _strlength(char *s)
+int _strlen(char *s)
 {
 	int count;
 
