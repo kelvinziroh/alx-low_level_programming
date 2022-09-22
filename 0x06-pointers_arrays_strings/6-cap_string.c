3eb5cb638b9b21c396cbc4d272ff7920a005c5bf
kelvinziroh <kelvin.ziroh@gmail.com>
2022-09-22 09:43:37 -0700

    Fix bug

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,7 +13,7 @@ char *cap_string(char *s)
 	int word_separators[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
 	if (*(s + count) >= 97 && *(s + count) <= 122)
-		*s(s + count) = *(s + count) - 32;
+		*(s + count) = *(s + count) - 32;
 
 	count++;
 	while (*(s + count) != '\0')
