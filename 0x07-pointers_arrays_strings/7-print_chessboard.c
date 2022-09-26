c76c5a6358eb1a8244249e40dc2ecad0f855ea9d
kelvinziroh <kelvin.ziroh@gmail.com>
2022-09-26 13:22:04 -0700

    Print chess board layout

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_chessboard - Display the chess board layout
+ * @a: Input memory location
+ * Return: Nothing.
+ */
+void print_chessboard(char (*a)[8])
+{
+	unsigned int count;
+	unsigned int count2;
+
+	count = 0;
+	while (count < 64)
+	{
+		if (count % 8 == 0 && count != 0)
+		{
+			count2 = count;
+			_putchar('\n');
+		}
+		_putchar(a[count / 8][count - m]);
+		count++;
+	}
+	_putchar('\n');
+}
