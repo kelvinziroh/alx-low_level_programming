d8bf2cf61360a790e52e80e3f5a4723894e055ae
kelvinziroh <kelvin.ziroh@gmail.com>
2022-09-26 10:23:03 -0700

    Find first occurence of a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strchr - locates the location of the first occurence of a
+ * character in a string.
+ * @s: the string.
+ * @c: the character.
+ *
+ * Return: the memory location to the first occurence of c.
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int count;
+
+	count = 0;
+	while (*(s + count) != '\0')
+	{
+		if (*(s + count) == c)
+			return (s + count);
+		count++;
+	}
+
+	if (*(s + count == c))
+		return (s + count);
+	return ('\0');
+}
